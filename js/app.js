27fd74983ba2299e7f41942a3872b99058b362db
Lindsay Nunez <lindsayanunez@gmail.com>
2018-09-04 22:20:10 -0400

    Create display function for number of clicks. Connect js file to html.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-let cat = document.getElementById('cat');
+function displayClicks(){
+  let clicks = 0;
+  let cat = document.getElementById('cat');
+  let clickCount = document.querySelector('.clickCount');
+
+  cat.addEventListener('click', function() {
+  clicks ++;
+  clickCount.innerHTML = clicks;
+  });
+}
+
+displayClicks();
