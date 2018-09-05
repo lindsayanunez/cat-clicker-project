4f481f89ec3e008c7253ec992673256280bd542d
Lindsay Nunez <lindsayanunez@gmail.com>
2018-09-04 22:55:40 -0400

    Added onload fucntions.

--- js/app.js
+++ js/app.js
@@ -18,6 +18,13 @@ let cat1 = new Cat('Aria','/img/cat1.jpg');
 let cat1 = new Cat('Leo','/img/cat2.jpg')
 
 document.addEventListener("DOMContentLoaded", function() {
+  let catArea1 = document.querySelector('.catArea1');
+  let catArea2 = document.querySelector('.catArea2');
 
+  catArea1.src = `${cat1.image}`;
+
+DOM_a.appendChild(DOM_img);
+
+  catArea1.appendChild('')
 
 });
